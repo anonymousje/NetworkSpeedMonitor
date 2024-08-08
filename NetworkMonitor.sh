5ccbe09e2c495fbeb9390e0e66d239a343d046d3
Foad Ahmed <158590457+anonymousje@users.noreply.github.com>
2024-08-08 19:46:57 +0500

    Update NetworkMonitor.sh

--- NetworkMonitor.sh
+++ NetworkMonitor.sh
@@ -4,7 +4,7 @@ while true; do
   # Run speedtest and capture output
   speedtest_output=$(speedtest-cli --simple)
   
-  # Process the output (replace with your logic)
+ 
   echo "Speedtest Results: $speedtest_output" >> speedtest_log.txt
   
   # Sleep for 5 minutes (adjust as needed)
